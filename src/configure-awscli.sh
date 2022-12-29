9e4778582187bda29675f8fb62741d79f517ed31
Caio Souza <caio.7s@outlook.com>
2022-12-29 01:15:24 -0300

    feat: Adicionando sudo em comandos de instalação do awscli

--- src/configure-awscli.sh
+++ src/configure-awscli.sh
@@ -1,25 +1,25 @@
 # Verifica se o Python já está instalado
 if ! command -v python3 &> /dev/null; then
   # Instala o Python 3
-  apt-get install -y python3
+  sudo apt-get install -y python3
 fi
 
 # Verifica se o pip (gerenciador de pacotes do Python) já está instalado
 if ! command -v pip3 &> /dev/null; then
   # Instala o pip
-  apt-get install -y python3-pip
+  sudo apt-get install -y python3-pip
 fi
 
 # Atualiza o pip
-pip3 install --upgrade pip
+sudo pip3 install --upgrade pip
 
 # Instala o AWS CLI
-pip3 install awscli
+sudo pip3 install awscli
 
 # Verifica se o AWS CLI Local já está instalado
 if ! command -v aws-cli-local &> /dev/null; then
   # Instala o AWS CLI Local
-  pip3 install awscli-local
+  sudo pip3 install awscli-local
 fi
 
 # Solicite as credenciais de acesso à sua conta na AWS
