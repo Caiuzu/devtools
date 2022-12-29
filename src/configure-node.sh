f76e71e2ab6921a076a263b9bb67ba42523e3990
Caio Souza <caio.7s@outlook.com>
2022-12-28 21:56:43 -0300

    feat: Adicionando novas verificações e funcionalidades no script de instalação do NVM/Node

--- src/configure-node.sh
+++ src/configure-node.sh
@@ -1,33 +1,41 @@
 # Verifica se o NVM já está instalado
 if [ -d "$HOME/.nvm" ]; then
-  echo "O NVM já está instalado."
-  exit 0
-fi
-
-# Instala o NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-# Carrega o NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-# Verifica se o Node já está instalado
-if nvm ls | grep -q "default ->"; then
-  echo "O Node já está instalado."
-  exit 0
-fi
-
-# Verifica se um parâmetro foi informado
-if [ "$#" -ne 1 ]; then
-  # Instala a última versão do Node
-  nvm install node
-else
+  read -p "O NVM já está instalado. Deseja alterar a versão? (S/n) " alterar
+  if [ "$alterar" != "S" ] && [ "$alterar" != "s" ]; then
+	  exit 0
+  fi
+  read -p "Informe a versão:" versao
+  echo "Buscando versão $versao..."
+  
+  # Carrega o NVM
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+  
   # Instala a versão informada como parâmetro
-  nvm install "$1"
-fi
+  nvm install "$versao"
 
-# Define a versão instalada como padrão
-nvm alias default node
+  # Define a versão instalada como padrão
+  nvm alias default "$versao"
+else
+  # Instala o NVM
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+  # Carrega o NVM
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+  # Verifica se um parâmetro foi informado
+  if [ "$#" -ne 1 ]; then
+    # Instala a última versão do Node
+    nvm install node
+  else
+    # Instala a versão informada como parâmetro
+    nvm install "$1"
+    
+    # Define a versão instalada como padrão
+    nvm alias default "$1"
+  fi
+fi
 
 # Verifica se a instalação foi bem-sucedida
 node -v
